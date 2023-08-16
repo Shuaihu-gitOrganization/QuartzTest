eb227e57d82113a8a5775426a41f61f47e061bcc
shuaihu.zhang <shuaihu.zhang@9fzt.com.cn>
2023-08-16 14:44:23 +0800

    add：自定义jwt认证过滤器

--- src/main/java/com/atlucky/springsecuritytest/filter/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/atlucky/springsecuritytest/filter/JwtAuthenticationTokenFilter.java
@@ -1,9 +1,67 @@
 package com.atlucky.springsecuritytest.filter;
 
+import com.atlucky.springsecuritytest.domain.LoginUser;
+import com.atlucky.springsecuritytest.utils.JwtUtils;
+import com.atlucky.springsecuritytest.utils.RedisCache;
+import io.jsonwebtoken.Claims;
+import io.netty.util.internal.StringUtil;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.annotation.Resource;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+
 /**
  * @Date 2023/8/16 14:09
  * @Author XiaoHu
- * @Description
+ * @Description 自定义jwt认证过滤器
  **/
-public class JwtAuthenticationTokenFilter {
+@Component
+public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+    @Resource
+    private RedisCache redisCache;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        //获取token
+        String token = request.getHeader("token");
+
+        if (!StringUtils.hasText(token)) {
+            //无token放行
+            filterChain.doFilter(request, response);
+            //响应返回的时候会走下面的校验token
+            return;
+        }
+
+        //解析token
+        String userid;
+        try {
+            Claims claims = JwtUtils.praseJwt(token);
+            userid = claims.getSubject();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("token非法");
+        }
+        //从Redis中获取用户信息
+        String redisKey = "login:" + userid;
+        LoginUser loginUser = redisCache.getCacheObject(redisKey);
+        if (Objects.isNull(loginUser)){
+            throw new RuntimeException("用户未登录");
+        }
+        //存储SecurityContextHolder中
+        //ToDo 获取权限信息封装到tAuthentication中
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                loginUser,null,null);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        //放行
+        filterChain.doFilter(request,response);
+
+    }
 }
