0b64ac61066de4e2dc972f8a7c715850db5e4d18
shuaihu.zhang <shuaihu.zhang@9fzt.com.cn>
2023-08-21 11:01:19 +0800

    初始化新模块

---
+++ QuartzTest/src/main/java/com/atlucky/quartztest/QuartzTestApplication.java
@@ -0,0 +1,13 @@
+package com.atlucky.quartztest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzTestApplication.class, args);
+	}
+
+}
